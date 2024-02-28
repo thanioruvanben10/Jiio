a4d0e6cea0fb8365c9b4928c7c9277a6b8e6287d
thanioruvanben10 <91356119+thanioruvanben10@users.noreply.github.com>
2024-02-28 20:29:44 +0530

    Update jiodownloader.py

--- jiodownloader.py
+++ jiodownloader.py
@@ -11,8 +11,6 @@ print('\ntest link: https://www.jiocinema.com/movies/sergeant-bhojpuri/3767689\n
 link = input('link: ')
 link_id = re.findall(r'.*/(.*)', link)[0].strip()
 
-m3u8DL_RE = 'N_m3u8DL-RE'
-
 def replace_invalid_chars(title: str) -> str:
     invalid_chars = {'<': '\u02c2', '>': '\u02c3',
     ':': '\u02d0', '"': '\u02ba', '/': '\u2044',
@@ -82,7 +80,7 @@ json_data2 = {
 }
 
 response2 = requests.post('https://apis-jiovoot.voot.com/playbackjv/v4/'+link_id+'', headers=headers2, json=json_data2, verify=False).json()
-
+print(response2)
 contentType = response2['data']['contentType']
 
 if contentType == 'MOVIE':
@@ -107,80 +105,3 @@ print(f'\n{title}\n')
 mpd = response2['data']['playbackUrls'][0]['url']
 
 lic_url = response2['data']['playbackUrls'][0]['licenseurl']
-
-try:
-    import requests
-    
-    headers03 = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'Connection': 'keep-alive',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
-    }
-    
-    response03 = requests.get(mpd, headers=headers03, verify=False).text
-    
-    pssh = re.findall(r'<cenc:pssh>(.{20,170})</cenc:pssh>', response03)[0].strip()
-    print(f'{pssh}\n')
-
-    import base64, requests, sys, xmltodict, json
-    from pywidevine.L3.cdm import cdm, deviceconfig
-    from base64 import b64encode
-    from pywidevine.L3.getPSSH import get_pssh
-    from pywidevine.L3.decrypt.wvdecryptcustom import WvDecrypt
-    import time
-    import re  
-    
-    headers = {
-        'authority': 'prod.media.jio.com',
-        'accesstoken': access_token,
-        'appname': appName,
-        'content-type': 'application/octet-stream',
-        'deviceid': deviceId,
-        'devicetype': 'Web',
-        'isdownload': 'false',
-        'origin': 'https://www.jiocinema.com',
-        'os': 'android',
-        'referer': 'https://www.jiocinema.com/',
-        'uniqueid': uniqueid,
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
-        'versioncode': '570',
-        'x-feature-code': 'ytvjywxwkn',
-        'x-platform': 'Web',
-        'x-playbackid': uniqueid,
-    }
-    
-    def WV_Function(pssh, lic_url, cert_b64=None):
-        wvdecrypt = WvDecrypt(init_data_b64=pssh, cert_data_b64=cert_b64, device=deviceconfig.device_android_generic)                   
-        widevine_license = requests.post(url=lic_url, data=wvdecrypt.get_challenge(), headers=headers, verify=False)
-        license_b64 = b64encode(widevine_license.content)
-        wvdecrypt.update_license(license_b64)
-        Correct, keyswvdecrypt = wvdecrypt.start_process()
-        if Correct:
-            return Correct, keyswvdecrypt
-    Correct, keys = WV_Function(pssh, lic_url)
-    
-    for key in keys:
-        print('--key ' + key)
-    
-    ke_ys = ' '.join([f'--key {key}' for key in keys]).split()
-    
-    print()
-    subprocess.run([m3u8DL_RE,
-                    '-M', 'format=mkv:muxer=ffmpeg',
-                    '--concurrent-download',
-                    '--log-level', 'INFO',
-                    '--save-name', 'video', mpd, *ke_ys])
-
-except IndexError: #DRM free
-    print()
-    subprocess.run([m3u8DL_RE,
-                    '-M', 'format=mkv:muxer=ffmpeg',
-                    '--concurrent-download',
-                    '--log-level', 'INFO',
-                    '--save-name', 'video', mpd])
-
-try:
-    Path('video.mkv').rename(''+title+'.mkv')
-    print(f'{title}.mkv \nall done!\n')
-except FileNotFoundError:
-    print("[ERROR] no mkv file")
